0715ff3edfd9fb3104caa0511ad627fa91c2f551
unknown <juanlecarosd@gmail.com>
2024-11-25 16:44:38 -0300

    arreglado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("almacem.json");
+const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000; //  chose port from here like 8080, 3001
 
